56d337441a0782528fe35190cb0895afe3258eff
raphaeltcf <raphaeltcf@gmail.com>
2023-05-10 17:03:21 -0300

    first commit

---
+++ extract.py
@@ -0,0 +1,42 @@
+import random
+import numpy as np
+import pickle
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+
+
+def clear_writing(writing):
+    sentence_words = nltk.word_tokenize(writing)
+    return [lemmatizer.lemmatize(words.lower()) for word in sentence_words]
+
+def bag_of_words(writing, words):
+    sentence_words = clear_writing(writing)
+    bag = [0]*len(words)
+    for setence in sentence_words:
+        for i, word in enumerate(words):
+            if word == setence:
+                bag[i] = 1
+    return(np.array(bag))
+
+def class_prediction(writing, model):
+    prevision = bag_of_words(writing, words)
+    response_prediction = model.predict(np.array([prevision]))[0]
+    results = [[index, response] for index, response in enumerate(response_prediction) if response > 0.25]
+    if "1" not in str(prevision) or len(results) == 0 : 
+        results = [[0, response_prediction[0]]]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return [{"intent": classes[r[0]], "probality": str(r[1])} for r in results]
+
+def get_response(intents, intents_json):
+    tag = intents[0]['intent']
+    list_of_intents = intents_json['intents']
+    for idx in list_of_intents:
+        if idx['tag'] == tag:
+            result = random.choice(idx['responses'])
+            break
+    return result
+
---
+++ train.py
@@ -0,0 +1,72 @@
+import json
+import pickle
+import nltk
+import random
+import numpy as np
+
+
+from nltk.stem import WordNetLemmatizer
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+
+nltk.download('punkt')
+nltk.download('wordnet')
+lemmatizer = WordNetLemmatizer()
+
+words = []
+documents = []
+intents = json.loads(open('intents.json', encoding='utf-8').read())
+classes = [i['tag'] for i in intents['intents']]
+ignore_words = ["!", "@", "#", "$", "%", "*", "?"]
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word = nltk.word_tokenize(pattern)
+        words.extend(word)
+        documents.append((word, intent['tag']))
+
+
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+
+words = sorted(list(set(words)))
+classes = sorted(list(set(classes)))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+training = []
+output_empty = [0] * len(classes)
+for document in documents:
+    bag = []
+    pattern_words = document[0]
+    pattern_words = [lemmatizer.lemmatize( word.lower()) for word in pattern_words]
+
+    for word in words:
+        bag.append(1) if word in pattern_words else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+
+x = list(training[:, 0])
+y = list(training[:, 1])
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(y[0]), activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+m = model.fit(np.array(x), np.array(y), epochs=200, batch_size=5, verbose=1)
+model.save('model.h5', m)
+
+print("fim")
